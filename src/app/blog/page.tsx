cada5f5f22d337ebdaba48d0bda04af994649af4
R1013-T <nosuke1013@icloud.com>
2023-11-26 19:24:37 +0900

    done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,19 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'ブログ',
+  title: "ブログ",
 };
 
 export default function Page() {
-  return <div>Blog</div>;
+  return (
+    <article className="border border-green-400 h-full p-4 mx-auto max-w-6xl overflow-hidden">
+      <div className="h-auto w-full border border-blue-500 hidden lg:grid grid-cols-4 gap-4">
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+      </div>
+    </article>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ export default function Home() {
   return (
     <article className="border border-green-400 h-full p-4 mx-auto max-w-6xl overflow-hidden">
       <div className="h-full w-full border border-blue-500 hidden lg:grid grid-cols-4 grid-rows-3 gap-4">
-        <div className="col-span-2 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">.div1</div>
-        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showTopBottom">.div2</div>
-        <div className="col-span-1 row-span-2 border border-white bg-white/40 rounded-md opacity-0 showRightLeft">.div3</div>
-        <div className="col-span-1 row-span-2 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">.div4</div>
-        <div className="col-span-2 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showBottomTop">.div5</div>
-        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showTopBottom">.div6</div>
-        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">.div7</div>
-        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showRightLeft">.div8</div>
+        <div className="col-span-2 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">プロフィール</div>
+        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showTopBottom">GitHub</div>
+        <div className="col-span-1 row-span-2 border border-white bg-white/40 rounded-md opacity-0 showRightLeft">画像</div>
+        <div className="col-span-1 row-span-2 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">スロット</div>
+        <div className="col-span-2 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showBottomTop">スキル</div>
+        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showTopBottom">X</div>
+        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showLeftRight">東京</div>
+        <div className="col-span-1 row-span-1 border border-white bg-white/40 rounded-md opacity-0 showRightLeft">メール</div>
       </div>
     </article>
   );
